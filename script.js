ea642804f66ddba6a3e625ee6ddef6221f2a30b9
Julian Soberon <ju.soberon@duocuc.cl>
2026-06-24 21:08:17 -0400

    se agrega el modo oscuro

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 const boton = document.getElementById("botonmensaje");
 const presentacion = document.getElementById("presentacion");
+const modo = document.getElementById("modo");
+
+modo.addEventListener("click", function() {
+    document.body.classList.toggle("dark-mode");
+    if (document.body.classList.contains("dark-mode")) {
+        modo.innerText = "modo claro";
+    }
+    else {
+        modo.innerText = "modo oscuro";
+    }
+});
 
 boton.addEventListener("click", function() {
-   if (presentacion.style.display === "none" || presentacion.style.display === "") {
+   if (presentacion.style.display === "" || presentacion.style.display === "none") {
         presentacion.style.display = "block";
         boton.innerText = "Ocultar mi presentación";
     }
